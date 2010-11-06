48c985c8aa690155ed58ff5cf8a98288ee3fa2f4
Saud Malik <smalik2@my.westga.edu>
2010-11-05 23:39:17 -0400

    Add pongball class.

---
+++ SaudPongXNA/SaudPongXNA/ui/PongBall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SaudPongXNA.ui
+{
+    class PongBall
+    {
+        public static Sprite createPongBall(Game game, SpriteBatch batch, Vector2 initialPosition, Vector2 initialVelocity)
+        {
+            Sprite sprite = new Sprite(game, "Images/ball", batch, initialPosition, initialVelocity);
+            return sprite;
+        }
+    }
+}
